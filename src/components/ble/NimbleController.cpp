a5222e34dfaa8fda9eeb070c92b28892349f61ab
Mikael Falkvidd <github@mjo.se>
2026-08-08 11:04:15 +0200

    Revert erroneous removal of comment

--- src/components/ble/NimbleController.cpp
+++ src/components/ble/NimbleController.cpp
@@ -211,6 +211,7 @@ int NimbleController::OnGAPEvent(ble_gap_event* event) {
         connectionHandle = event->connect.conn_handle;
         bleController.Connect();
         systemTask.PushMessage(Pinetime::System::Messages::BleConnected);
+        // Service discovery is deferred via systemtask
         ble_gap_security_initiate(event->connect.conn_handle);
       }
       break;
